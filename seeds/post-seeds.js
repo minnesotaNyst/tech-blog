f7511a2a7539c424507ac1035a9d43cf14164a96
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-03-11 20:00:58 -0600

    added post-seeds per boilerplate from module

---
+++ seeds/post-seeds.js
@@ -0,0 +1,40 @@
+const { Post } = require('../models');
+
+const postdata = [
+	{
+		title: 'Donec posuere metus vitae ipsum.',
+		content: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+		user_id: 1
+	},
+	{
+		title: 'Morbi non quam nec dui luctus rutrum.',
+		content: 'https://nasa.gov/donec.json',
+		user_id: 2
+	},
+	{
+		title:
+			'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+		content:
+			'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+		user_id: 3
+	},
+	{
+		title: 'Nunc purus.',
+		content: 'http://desdev.cn/enim/blandit/mi.jpg',
+		user_id: 3
+	},
+	{
+		title: 'Pellentesque eget nunc.',
+		content: 'http://google.ca/nam/nulla/integer.aspx',
+		user_id: 2
+	},
+	{
+		title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+		content: 'https://stanford.edu/consequat.png',
+		user_id: 1
+	}
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
